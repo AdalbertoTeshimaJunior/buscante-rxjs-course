35fe125b697ca74de8b83a044b784ff38845caa7
Adalberto Teshima Junior <teshima.junior@granitosistemas.com.br>
2023-02-06 10:17:42 -0300

    Updating service. adding interfaces.

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,3 +1,4 @@
+import { LivrosResultado } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -11,7 +12,7 @@ export class BookService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  fetch(valueTyped: string): Observable<any> {
+  fetch(valueTyped: string): Observable<LivrosResultado> {
       const params = new HttpParams().append('q', valueTyped)
       return this.http.get(this.API, { params })
   }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,24 +1,31 @@
 import { BookService } from './../../service/book.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listBooks: [];
   searchField: string = '';
+  subscription: Subscription;
 
   constructor(private service: BookService) { }
 
   fetchBooks() {
-    this.service.fetch(this.searchField).subscribe((APIreturn) => {
-
+    this.subscription = this.service.fetch(this.searchField).subscribe({
+      next: APIreturn => console.log(APIreturn),
+      error: error => console.error(error),
+      complete: () => console.log("Completed"),
     });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
 
 
